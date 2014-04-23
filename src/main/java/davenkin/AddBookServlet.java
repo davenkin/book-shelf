c1a103476e36658664b446d218acc32e76608729
davenkin <davenkin@gmail.com>
2014-04-23 20:10:12 +0800

    form for adding books

--- src/main/java/davenkin/AddBookServlet.java
+++ src/main/java/davenkin/AddBookServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by twer on 4/23/14.
@@ -12,6 +13,28 @@ public class AddBookServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
 
-        response.getWriter().println("<h1>" + "Book Shelf" + "</h1>");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Add Book</title></head>");
+        out.println("<body>");
+        out.println("<h1>Add a new book:</h1>");
+        out.println("<form action=\"add\" method=\"post\">");
+        out.println("ISBN: <input type=\"text\" name=\"isbn\"><br>");
+        out.println("Name: <input type=\"text\" name=\"name\"><br>");
+        out.println("Price: <input type=\"text\" name=\"price\"><br>");
+        out.println("Author: <input type=\"text\" name=\"author\"><br>");
+        out.println(" <input type=\"submit\"> ");
+        out.println("</form");
+        out.println("</body>");
+        out.println("</html>");
+
     }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println(request.getParameter("name")+" added");
+    }
+
 }
